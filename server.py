5cc2e835989901997271a5e3af290794404deced
Alec Kenzo <aleckenzo@hotmail.com>
2025-05-20 15:26:45 -0300

    feat: configura backend FastAPI para servir HTML na rota '/' com Jinja2

---
+++ server.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+# Monta a pasta de arquivos estáticos (CSS, JS, imagens)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
